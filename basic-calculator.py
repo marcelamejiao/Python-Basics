1b721f0dc6ea18931aba93e55fcff5a74bffaffd
marcelamejiao <mejiao24@hotmail.com>
2024-02-22 17:43:15 +1100

    create basic calculator to perform adittion using input function and converting variables

---
+++ basic-calculator.py
@@ -0,0 +1,6 @@
+# Basic calculator converting variables types
+
+First = input('Enter the first number: ')
+Second = input('Enter the second number: ')
+Sum = float(First) + float(Second)
+print(Sum)
