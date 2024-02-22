57dc35f55f71d9770257cf24b4e29d4de669d7a8
marcelamejiao <mejiao24@hotmail.com>
2024-02-22 17:16:34 +1100

    beggining lesson of variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Declaring variables:
+
+# Numbers
+age = 20
+temperature = 30
+price = 19.95
+
+# Strings
+# Can use single or double quotes
+first_name = "Marcela"
+
+# Boolean
+
+is_online = True
+is_on = False
+
+# Print in the console:
+print(age)
+
+
+# Exercise : create variables for a patient named John Smith, 20 years old and is a new patient:
+
+patient_name = 'John Smith'
+patient_age = 20
+is_new_patient = True
