55a574d6990c738b4cb6f5cc74c20458a24577e7
marcelamejiao <mejiao24@hotmail.com>
2024-02-22 17:41:18 +1100

    lesson of convert variables types

---
+++ convert-variables.py
@@ -0,0 +1,14 @@
+# Convert variables type
+
+birth_year = input('Enter your birth year: ')
+
+# To convert a string into an integer, it needs to be wrapped in a int() function
+my_age = 2024 - int(birth_year)
+
+print(my_age)
+
+# another types:
+# float()
+# bool()
+# str()
+# int()
