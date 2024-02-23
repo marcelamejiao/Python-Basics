a526fce05180f27579eb27aabf05ae699e8d8e9c
marcelamejiao <mejiao24@hotmail.com>
2024-02-23 16:17:50 +1100

    string methods

---
+++ str-methods.py
@@ -0,0 +1,24 @@
+# A function that is part of an object  is called method
+
+# If it does not find return -1
+# Methods return new strings
+
+course = 'Python for beginners'
+print(course.upper())
+print(course)
+
+# Return the index of the first occurrence
+# Return index 1
+print(course.find('y'))
+
+# Return the index of the word: for
+# Return 7
+print(course.find('for'))
+
+# Replace method
+print(course.replace('for', '4'))
+
+# in operator
+# Returns a boolean = True
+print('Python' in course)
+
