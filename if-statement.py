bae41f03c897541477936f74ebf5df60ee29302f
marcelamejiao <mejiao24@hotmail.com>
2024-02-23 17:36:17 +1100

    if statement format

---
+++ if-statement.py
@@ -0,0 +1,18 @@
+# If statement
+
+temperature = 8
+
+if temperature > 30:
+    print("It's a hor day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+    print("Carry a jacket")
+print("Done")
+
+
+
