c5bfbccf825e49ab2128e3fdfa357972ce046727
marcelamejiao <mejiao24@hotmail.com>
2024-02-23 17:08:57 +1100

    arithmetic operators

---
+++ arithmetic-operators.py
@@ -0,0 +1,33 @@
+# add +, subtract -, * multiply
+# One / returns a number with decimals - floating
+# Two // returns a round number - integer
+# % modulos
+# exponent operator **
+
+# =+ argument assignment operator
+x = 10
+x += 3
+
+# Operator precedence
+x = (10 + 3) * 2
+
+
+# Comparison operators
+
+# Boolean
+# < > <= >=
+# Equality operator ==
+# Not equality operator !=
+
+
+# Logical operators
+
+# and operator
+price = 25
+print(price > 10 and price < 30)
+
+# or operator
+print(price > 10 or price < 20)
+
+# not operator inverses any value
+print(not price > 10)
